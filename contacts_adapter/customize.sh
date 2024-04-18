e824e9d7af99aaed6a66c6479d4e08c906db3bd6
FalconSN <110735449+FalconSN@users.noreply.github.com>
2024-04-18 20:27:33 +0300

    Update customize.sh
    Remove unnecessary chmod

--- contacts_adapter/customize.sh
+++ contacts_adapter/customize.sh
@@ -1,4 +1,3 @@
 chown -R 0:0 $MODPATH/system
 chmod -R 755 $MODPATH/system
 chmod 644 $MODPATH/system/app/GoogleContactsSyncAdapter/GoogleContactsSyncAdapter.apk
-chmod +x $MODPATH/boot-completed.sh
